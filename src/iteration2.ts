2cac0f2d3cb9310fcd84a060be0d02d26acaf3eb
Pau Serrano <pauserrano14@gmail.com>
2026-01-30 14:20:09 +0100

    LAB done

--- src/iteration2.ts
+++ src/iteration2.ts
@@ -1,5 +1,29 @@
-//
-// Iteration 2 | Functions
-//
+function calcMultiplication(a: number, b: number): number {
+    return a * b
+}
 
+const result1 = calcMultiplication(2, 10)
+console.log(result1)
 
+function isEven(n: number): boolean {
+    if (n % 2 === 0) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+function calcArrayAverage(numbersArr: number[]): number | string {
+    if (numbersArr.length === 0){
+        return "Please provide an array with at least one element"
+    }
+    const total = numbersArr.reduce((accumulator, currentValue) => {
+          return accumulator + currentValue;
+      });
+
+      const average = total / numbersArr.length;
+
+      return average;
+
+}
